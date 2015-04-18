a4faebf42916d698cffa2b48ef4990ff80b3f577
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-18 19:52:44 -0400

    logging

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,8 +1,10 @@
 $( function() {
   $("body").hammer().on("swiperight", function (e) {
     $('.left.carousel-control').click();
+    console.log("swiped right");
   }).on("swipeleft", function (e) {
     $('.right.carousel-control').click();
+    console.log("swiped left");
   });
 });
 
