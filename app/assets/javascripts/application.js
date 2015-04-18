e78b5c2e1a6a4638efae802d50b31345b889b0d1
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-18 16:03:38 -0400

    hammerjs-rails in asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require hammer
 //= require bootstrap-sprockets
 //= require_tree .
+//= require_tree ../../../vendor/assets/javascripts/.
+
