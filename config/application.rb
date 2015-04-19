2be32750c52b1012c334d7cf8c13d78073edf734
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-19 17:28:29 -0400

    xframe options and br's in pregnancy texts

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module CommunityResourcesOnline
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => ''
+    }
+
   end
 end
