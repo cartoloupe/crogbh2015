1f3603a29e27511f6c665f56d361ab1a6bab38d7
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-19 17:01:36 -0400

    iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  after_filter :allow_iframe
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def allow_iframe
+    response.headers.delete "X-Frame-Options"
+  end
 end
