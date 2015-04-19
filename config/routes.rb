d9100f975b0dd27142fb223d802d91bca8ca96e5
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-19 16:14:05 -0400

    change routes around

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'pitch#index'
+  root 'welcome#index'
+
+  get 'mockup', to: 'pitch#index'
 
   get 'pitch/index'
 
