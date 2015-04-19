de95612d1c1e3843b28c431343ac683be5269363
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-19 00:14:24 -0400

    add image 8 and refactor to pitch controller

--- app/controllers/pitch_controller.rb
+++ app/controllers/pitch_controller.rb
@@ -1,4 +1,17 @@
 class PitchController < ApplicationController
   def index
+    #@images = (1..8).map{|n|
+    #  "CRO#{sprintf "%03d", n}.png"
+    #}
+    @images = %w(
+      CRO001.png
+      CRO002.png
+      CRO003.png
+      CRO004.png
+      CRO005.png
+      CRO006.png
+      CRO007.png
+      CRO008.png
+    )
   end
 end
