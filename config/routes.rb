f5cde58e442d1be713b410d798e1d736c8a15be7
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-18 21:45:06 -0400

    set root to point to demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # You can have the root of your site routed with "root"
+  root 'pitch#index'
+
   get 'pitch/index'
 
   get 'pregnancy/index'
@@ -12,8 +15,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
