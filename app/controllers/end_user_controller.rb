efd09e4e372b657e5ee3e75460bf70e390026a17
Robert Lude <robert.lude@gmail.com>
2015-07-01 18:31:17 -0400

    WIP

--- app/controllers/end_user_controller.rb
+++ app/controllers/end_user_controller.rb
@@ -1,7 +1,41 @@
 class EndUserController < ApplicationController
   def new
-    @user = User.new
-    @end_user_detail = EndUserDetail.new
+    @user   = User.new
+    @detail = EndUserDetail.new
     render 'end_user_form'
+  end
+
+  def create
+    @detail = EndUserDetail.new detail_parameters
+    @user   = User.new user_parameters.merge(detail: @detail)
+    if @detail.valid? && @user.valid?
+      @detail.save
+      @user.save
+      redirect_to root_path
+    else
+      render 'end_user_form'
+    end
+  end
+
+  private
+
+  def user_parameters
+    @user_parameters ||= params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :first_name,
+      :last_name,
+    )
+  end
+
+  def detail_parameters
+    @detail_parameters ||= params.require(:detail).permit(
+      :gender,
+      :phone_number,
+      :address,
+      :city,
+      :state,
+    )
   end
 end
